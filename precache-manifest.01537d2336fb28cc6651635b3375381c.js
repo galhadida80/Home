29f5f0f8e0c06339f82ce056a0a8266bd368e44e
גל חדידה <hadidd.gal@msmail.ariel.ac.il>
2021-01-25 17:18:10 +0200

    Updates

--- precache-manifest.01537d2336fb28cc6651635b3375381c.js
+++ precache-manifest.01537d2336fb28cc6651635b3375381c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a7d57ccc6877a5d5187d46efaaa176f7",
+    "revision": "14231d7ab71dc5fa0e0e7ae40bbd6a79",
     "url": "/home/index.html"
   },
   {
-    "revision": "0bca44448c6ec88a61e8",
+    "revision": "9e33c363f9676c409109",
     "url": "/home/static/css/main.022eb82c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0bca44448c6ec88a61e8",
-    "url": "/home/static/js/main.56a8725f.chunk.js"
+    "revision": "9e33c363f9676c409109",
+    "url": "/home/static/js/main.5eb45207.chunk.js"
   },
   {
     "revision": "c0578b487099793a068c",
